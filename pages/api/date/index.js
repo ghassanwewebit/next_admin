54748da049b6af98c57554e5f65070defa391fb0
wewebit <g.ghanimah@wewebit.com>
2022-08-29 10:58:50 +0300

    loginUpdate

--- pages/api/date/index.js
+++ pages/api/date/index.js
@@ -3,5 +3,5 @@
 export default  async function handler(req,res){
     const date= new Date()
 
-    res.status(200),json({title:"this page are working ",date:date})
+    res.status(200).json({title:"this page are working ",date:date})
 }
