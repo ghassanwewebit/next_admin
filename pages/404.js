e4244597fcc45086a780cff51c795123ecd76852
wewebit <g.ghanimah@wewebit.com>
2022-08-30 11:49:45 +0300

    update

--- pages/404.js
+++ pages/404.js
@@ -1,23 +1,28 @@
 
 import Link from "next/link"
+import Image from "next/image"
+import Rightimage from'./../public/img/right-shape.png'
+import leftimage from "./../public/img/image-left.png"
 export default function AdminError(){
 
 
     return(
 
-        <div id="notfound">
-		<div className="notfound-bg"></div>
-		<div className="notfound">
-			<div className="notfound-404">
-				<h1>404</h1>
-			</div>
-			<h2>we are sorry, but the page you requested was not found</h2>
-			<Link href="/adminll" className="home-btn">Go Home</Link>
-			<Link href="https://wewebit.com" className="contact-btn">Contact us</Link>
-			<div className="notfound-social">
-		
-			</div>
-		</div>
-	</div>
+<div className="wrapper h-100 mt-5-wrapper">
+  <div className="container ">
+    <div className="grid-row">
+      <div className="colmun colmun-left">
+		<Image src={leftimage} alt="image-left"/>
+        <h1 className="px-spc-b-20">We can t find the page you are looking for.</h1>
+        <span className="px-spc-b-20">This page has been relocated or removed.</span>
+        <button className="go-home"><i className="fa fa-home"></i> <Link href='/'> Go Home</Link></button>
+      </div>
+      <div className="colmun colmun-right">
+		<Image src={Rightimage} alt="image-left"/>
+
+      </div>
+    </div>
+  </div>
+</div>
     )
 }
